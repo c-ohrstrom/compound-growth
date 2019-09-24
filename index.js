98c44b4a8e0ae10979134fbb125c4b9d0ddd5488
C-Ohrstrom <christoffer.j.ohrstrom@gmail.com>
2019-09-24 20:42:40 +0200

    Made the first html and js
    Created user inputs and started making the calculating method

---
+++ index.js
@@ -0,0 +1,15 @@
+function CalculateCompoundGrowth(startCapital, monthlySavings, yearsForCompound, dividends) {
+    let yearlyGainsList = [];
+    let capital = startCapital * dividends + (monthlySavings * 12);
+    for (let i = 0; i < yearsForCompound; i++) {
+        
+        capital += capital * dividends;
+        capital += monthlySavings * 12;
+        let year = {
+            year: i,
+            growth: capital
+        };
+        yearlyGainsList.push(year)
+    }
+    return yearlyGainsList;
+}
