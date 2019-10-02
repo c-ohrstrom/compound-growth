eb6545e1a1fa7de21a6813df263c4aa9cdb4bbdf
C-Ohrstrom <christoffer.j.ohrstrom@gmail.com>
2019-10-02 22:05:41 +0200

    Improvements
    Made some improvments to the calculating method and made the results visible in the browser

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
-function CalculateCompoundGrowth(startCapital, monthlySavings, yearsForCompound, expectedGrowth, dividends, doesReinvest) {
+let button = document.querySelector("#calculate-compound");
+
+button.addEventListener("click", () => {
+    // let startingCapital = document.querySelector("#starting-capital");
+    // let monthlySavings = document.querySelector("#monthly-saving").value;
+    // let dividendRate = document.querySelector("#dividendRate");
+    let years = document.querySelector("#years-for-growth").value;
+    let result = CalculateCompoundGrowth(100000, 1000, years, 0.07, 0.04, false);
+    PrintCompound(result);
+});
+
+function PrintCompound(result) {
+    let tableBody = "";
+    let capital = 0;
+    result.forEach(element => {
+        capital += element.growth + element.dividend;
+        let tr = `<tr>
+            <td>${element.year}</td>
+            <td>${Math.round(element.growth)}</td>
+            <td>${Math.round(element.dividend)}</td>
+            <td>${Math.round(capital)}</td>
+        </tr>`;
+        tableBody += tr;
+    });
+    let table = document.querySelector("#compound-result tbody");
+    table.innerHTML = "";
+    table.innerHTML = tableBody;
+}
+
+function CalculateCompoundGrowth(startCapital, monthlySavings, yearsForCompound, expectedYearlyGrowth, dividends, doesReinvest) {
     let yearlyGainsList = [];
     let capital = startCapital * dividends + (monthlySavings * 12);
-    for (let i = 0; i < yearsForCompound; i++) {
-        
-        capital += capital * dividends;
-        capital += monthlySavings * 12;
+    for (let i = 1; i <= yearsForCompound; i++) {
+
+        let dividend = capital * dividends;
+        let yearlySaving = monthlySavings * 12;
+        let thisYearsGrowth = yearlySaving + dividend;
+        capital += thisYearsGrowth;
         let year = {
             year: i,
-            growth: capital
+            growth: thisYearsGrowth,
+            dividend: dividend
         };
+        console.log(`Yearly growth: ${thisYearsGrowth}
+        dividends: ${dividend}
+        `);
         yearlyGainsList.push(year)
     }
     return yearlyGainsList;
